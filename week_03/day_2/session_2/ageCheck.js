feca55b58ab9b6fcbd6dc149556794e9f67e27aa
srivatsava <venkatasrinu3@gmail.com>
2020-09-24 20:44:50 +0530

    W3_D2_S2

---
+++ week_03/day_2/session_2/ageCheck.js
@@ -0,0 +1,10 @@
+var age = 16
+if (age > 18) {
+    console.log('You can drive in India!')
+}
+else if (age < 18){
+    console.log("You can't drive in India")
+}
+else{
+    console.log('You are just old enough to drive in India!')
+}
