3201c504f1ff3750a747b30ea8feff31c5b761b3
srivatsava <venkatasrinu3@gmail.com>
2020-09-27 14:20:40 +0530

    W3_D2_S2

---
+++ week_03/day_2/session_2/countries.js
@@ -0,0 +1,50 @@
+var country = 'Nepal'
+switch(country){
+    case 'India':
+    console.log(country= 'The continent is Asia');
+    break;
+    case 'United states of America':
+    console.log(country='The continent is North America')
+    break;
+    case 'China':
+    console.log(country='The continent is Asia')
+    break;
+    case 'Nepal':
+    console.log(country='The continent is Asia')
+    break;
+    case 'Australia':
+   console.log(country='The continent is Oceania')
+    break;
+    case 'France':
+   console.log(country='The continent is Europe')
+    break;
+    case Germany:
+    console.log(country='The continent is Europe')
+    break;
+    case 'SouthAfrica':
+    console.log(country='The continent is Africa')
+    break;
+    case 'Nigeria':
+    console.log(country='The continent is Africa')
+    break;
+    case 'Mexico':
+    console.log(country='The continent is North America')
+    break;
+    case 'Argentinia':
+    console.log(country='The continent is South America')
+    break;
+    case 'Brazil':
+    console.log(country='The continent is South America')
+    break;
+    case 'Canada':
+    console.log(country='The continent is North America')
+    break;
+    case 'Singapore':
+    console.log(country='The continent is Asia')
+    break;
+    case 'Sweden':
+    console.log(country='The continent is Europe')    
+    default:
+   console.log(country='The continent is antarctica')
+
+}
