70e81d815a6bdd6edd8d2e200916654450084b46
srivatsava <venkatasrinu3@gmail.com>
2020-10-05 13:42:02 +0530

    W3_D3_S2

--- week_03/day_3/session_2/split_string.js
+++ week_03/day_3/session_2/split_string.js
@@ -1,13 +1,15 @@
-let str = 'Peetu*is*a*male*talent*intelligent*guy'
-let split_char = '*'
-let res = ''
-let arr = []
-for(let i=0;i<str.length;i++){
-    if(str[i] != split_char){
-        res+=str[i]
-    }else{
-        arr.push(res)
-        res=''
-    }
+var str = 'html is not a progrmming language '
+var split_char = ' '
+var res = ''
+var arr = []
+for (i=0;i<str.length;i++){
+  if(str[i] != split_char){
+    res+=str[i]
+  }
+  else{
+    arr.push(res)
+    res = ''
+  }
 }
 console.log(arr)
+
